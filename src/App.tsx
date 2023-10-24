5387242770ba801f03ae5b029d20acd760070ffc
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-24 16:27:39 +0700

    update ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { initialTravelPlan } from './places';
 
-function App() {
+interface Place {
+  id: number;
+  title: string;
+  childIds: number[];
+}
+
+interface TravelPlanState {
+  [key: number]: Place;
+}
+
+interface PlaceTreeProps {
+  id: number;
+  parentId: number;
+  placesById: TravelPlanState;
+  onComplete: (parentId: number, childId: number) => void;
+}
+
+export default function TravelPlan() {
+  const [plan, setPlan] = useState<TravelPlanState>(initialTravelPlan);
+
+  function handleComplete(parentId: number, childId: number): void {
+    const parent = plan[parentId];
+    const nextParent: Place = {
+      ...parent,
+      childIds: parent.childIds
+      .filter((id) => id !== childId),
+    };
+    setPlan({
+      ...plan,
+      [parentId]: nextParent,
+    });
+  }
+
+  const root = plan[0];
+  const planetIds = root.childIds;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Places to visit</h2>
+      <ol>
+        {planetIds.map((id) => (
+          <PlaceTree
+            key={id}
+            id={id}
+            parentId={0}
+            placesById={plan}
+            onComplete={handleComplete}
+          />
+        ))}
+      </ol>
+    </>
   );
 }
 
-export default App;
+function PlaceTree({ id, parentId, placesById, onComplete }: PlaceTreeProps) {
+  const place = placesById[id];
+  const childIds = place.childIds;
+  return (
+    <li>
+      {place.title}
+      <button onClick={() => onComplete(parentId, id)}>Complete</button>
+      {childIds.length > 0 && (
+        <ol>
+          {childIds.map((childId) => (
+            <PlaceTree
+              key={childId}
+              id={childId}
+              parentId={id}
+              placesById={placesById}
+              onComplete={onComplete}
+            />
+          ))}
+        </ol>
+      )}
+    </li>
+  );
+}
